8c22a436cc513ba3e3f825dd03f262de35592db1
Augustin Affognon <augustinaffognon2000@gmail.com>
2026-09-07 18:36:14 +0000

    Edit useApi.ts

--- app/composables/useApi.ts
+++ app/composables/useApi.ts
@@ -13,7 +13,7 @@
  */
 export const useApi = () => {
   const config = useRuntimeConfig()
-  const baseURL: string = (config.public.apiBase as string) || 'https://zemy.erika-app.com/api'
+  const baseURL: string = (config.public.apiBase as string) || 'https://zemybackend.sinustic.com/api'
   const authCookie = useCookie('admin_token')
 
   const fetchApi = <T = any>(endpoint: string, options: Record<string, any> = {}) => {
